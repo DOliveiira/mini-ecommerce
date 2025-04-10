13f1b7163447defe23a4b6bc90e33e7c3cd76f6a
Marcos Danilo de Oliveira <marcos.danilo@Inbrands.corp>
2025-04-10 15:03:58 -0300

    Configuração do banco de dados (MySql)

--- backend/index.js
+++ backend/index.js
@@ -14,3 +14,15 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
+
+const sequelize = require('./config/db');
+
+async function testConnection() {
+  try {
+    await sequelize.authenticate();
+    console.log('✅ Conexão com o MySQL estabelecida!');
+  } catch (error) {
+    console.error('❌ Falha ao conectar ao MySQL:', error);
+  }
+}
+testConnection();
