46d848fb27084c84f82c35cec5cd0913dcc07d06
Marcos Danilo de Oliveira <marcos.danilo@Inbrands.corp>
2025-04-14 10:47:35 -0300

    Modelo do produto adicioado, definindo a estrutura da tabela no DB

---
+++ backend/models/Product.js
@@ -0,0 +1,13 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+
+const Product = sequelize.define('Product', {
+    name: { type: DataTypes.STRING, allowNull: false },
+    price: { type: DataTypes.FLOAT, allowNull: false },
+    description: { type: DataTypes.TEXT, allowNull: true },
+    imageUrl: { type: DataTypes.STRING, allowNull: true }
+}, {
+    timestamps: true
+});
+
+module.exports = Product;
